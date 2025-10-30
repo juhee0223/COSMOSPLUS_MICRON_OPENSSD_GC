00a1f8387bcb16d0b92eb392ba52379f017f0672
juhee0223 <pjuhee23@dankook.ac.kr>
2025-10-30 14:30:25 +0900

    initial commit

---
+++ Debug/src/main.d
@@ -0,0 +1,71 @@
+src/main.o src/main.o: ../src/main.c \
+ ../../run-gftl3_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../run-gftl3_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../run-gftl3_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../run-gftl3_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../run-gftl3_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../run-gftl3_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../run-gftl3_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../run-gftl3_bsp/ps7_cortexa9_0/include/xil_mmu.h \
+ ../../run-gftl3_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../run-gftl3_bsp/ps7_cortexa9_0/include/xscugic_hw.h \
+ ../../run-gftl3_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../run-gftl3_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../run-gftl3_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../run-gftl3_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../run-gftl3_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../run-gftl3_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../run-gftl3_bsp/ps7_cortexa9_0/include/xscugic.h \
+ ../../run-gftl3_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../run-gftl3_bsp/ps7_cortexa9_0/include/xscugic_hw.h \
+ ../../run-gftl3_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../src/nvme/debug.h ../src/nvme/nvme.h ../src/nvme/nvme_main.h \
+ ../src/nvme/host_lld.h
+
+../../run-gftl3_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../run-gftl3_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../run-gftl3_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../run-gftl3_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../run-gftl3_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../run-gftl3_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../run-gftl3_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../run-gftl3_bsp/ps7_cortexa9_0/include/xil_mmu.h:
+
+../../run-gftl3_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../run-gftl3_bsp/ps7_cortexa9_0/include/xscugic_hw.h:
+
+../../run-gftl3_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../run-gftl3_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../run-gftl3_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../run-gftl3_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../run-gftl3_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../run-gftl3_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../run-gftl3_bsp/ps7_cortexa9_0/include/xscugic.h:
+
+../../run-gftl3_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../run-gftl3_bsp/ps7_cortexa9_0/include/xscugic_hw.h:
+
+../../run-gftl3_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../src/nvme/debug.h:
+
+../src/nvme/nvme.h:
+
+../src/nvme/nvme_main.h:
+
+../src/nvme/host_lld.h:
